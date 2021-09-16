309f13023c39297436b90122b4354df97c8b79a8
jillsian-codes <jftaylor@smu.edu>
2021-09-16 02:14:10 -0500

    created the three classes

---
+++ ProjectTwo/Bot.js
@@ -0,0 +1,14 @@
+class Bot{
+    constructor(speed, size, color, pos) {//only one allowed
+        //each bot needs to have a speed, a size, a color, and a starting position
+        this.speed = speed; //single value, randomized
+        this.size = size; //single value
+        this.color = color; //single value, randomized
+        this.pos = pos; //a vector, originally the starting point
+    }
+
+    draw(){
+
+    }
+
+}
